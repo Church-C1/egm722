ee431a0a4434bffcbad662a877dabcf672a4d78d
Carrie Church <your_email@example.comChurch-C1@ulster.ac.uk>
2026-03-15 06:03:22 +0000

    Fixed input type mismatch (converted string input to integer)

--- Week1/debugging_exercise.py
+++ Week1/debugging_exercise.py
@@ -4,7 +4,7 @@ import random
 rand = random.randint(1, 100)
 
 print('Guess a number between 1 and 20.')
-guess = int(input())  # number needs to be an integer
+guess = int(input()) # number needs to be an integer
 
 while guess != rand:  # if the guess is not equal to the random number, you have to guess again
     if guess > rand:  # if the guess is too high, tell the user.
@@ -13,6 +13,6 @@ while guess != rand:  # if the guess is not equal to the random number, you have
         print('Too high. Guess again.')
 
     print('Enter a new guess: ')
-    guess = input()
+    guess = int(input())
 
 print('You got it! The number was {}'.format(rand))
